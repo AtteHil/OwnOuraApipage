9e7a9adee114ed4b5436d9e591f02bfceaa935c0
Atte <attehiltunen@hotmail.com>
2023-11-23 20:13:08 +0200

    start of developing sleep data feature

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 // sends users personal access token to server side and server responds with user data as json object
 const fetchApiData =()=>{
     const token = document.getElementById('PersonalToken').value
-    fetch(`http://localhost:3000/OuraData/${token}`)
+    fetch(`http://localhost:3000/info/${token}`)
     .then(response => {
         if (!response.ok) {
             console.error("There was error while fetching");
@@ -11,8 +11,10 @@ const fetchApiData =()=>{
         .then(data =>{
             console.log(data.age);
             PersonalInfoDiv(data);
+            fetchSleepData();
 
         })
+    
 
 }
 
@@ -27,3 +29,19 @@ const PersonalInfoDiv=(data)=>{
     <p>Email: ${data.email}</p>`;
 
 }
+
+const fetchSleepData = () =>{
+    fetch(`/OuraData/sleep`)
+    .then(response => {
+        if (!response.ok) {
+            console.error("There was error while fetching");
+        }
+        return response.json(); 
+        })
+        .then(data =>{
+            console.log(data);
+            
+
+        })
+}
+;
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ let personalToken = ''
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'OuraApi' });
 });
-router.get('/OuraData/:token', function(req, res, next) {
+router.get('/info/:token', function(req, res, next) {
   personalToken = req.params.token;
-  console.log(personalToken);
+  
   var myHeaders = new Headers(); 
   myHeaders.append('Authorization', `Bearer ${personalToken}`); 
   var requestOptions = { 
@@ -18,6 +18,32 @@ router.get('/OuraData/:token', function(req, res, next) {
     .then(response => response.text()) 
     .then(result => {
       console.log(result)
+      res.send(result)}
+      ) 
+    .catch(error => console.log('error', error));
+});
+// function to get sleepdata for last nights sleep
+// now it gets 2 nights ago sleep score. with id more data of the sleep could be get. Have to test if this works for the lasty night also. 
+//(Now my oura doesn't have stats for the last night)
+router.get('/OuraData/sleep', function(req, res, next) {
+  
+  let today = new Date();
+  let yesterday = new Date(today.getTime() - 86400000)
+  let dayBefore = new Date(yesterday.getTime() -86400000)
+
+  
+  console.log(yesterday.toISOString().slice(0,10))
+  
+  var myHeaders = new Headers(); 
+  myHeaders.append('Authorization', `Bearer ${personalToken}`); 
+  var requestOptions = { 
+  method: 'GET', 
+  headers: myHeaders, 
+  };
+  fetch(`https://api.ouraring.com/v2/usercollection/daily_sleep?start_date=${dayBefore.toISOString().slice(0,10)}&end_date=${yesterday.toISOString().slice(0,10)}`, requestOptions) 
+    .then(response => response.text()) 
+    .then(result => {
+      
       res.send(result)}
       ) 
     .catch(error => console.log('error', error));
